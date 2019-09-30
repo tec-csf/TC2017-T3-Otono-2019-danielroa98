e7dc9f659b5cf77fb9ef02d032dab32a58aa266e
danielroa98 <daniel.roa98@gmail.com>
2019-09-30 15:02:39 -0500

    Base para cada código

---
+++ Camionero.cpp
@@ -0,0 +1,19 @@
+/*
+ *  El Camionero con Prisa
+ *  Daniel Roa
+ *  A01021960
+ *  Entrega: 4 de octubre del 2019
+ */
+
+#include <iostream>
+#include <stdio.h>
+#include <bits/stdc++.h>
+
+int main() {
+
+  int km;       //distancia a recorrer
+  int paradas;  //
+  int viaje;
+
+  return 0;
+}
