c97d072f7be625ed480bdc138ba07c6001babf76
danielroa98 <daniel.roa98@gmail.com>
2019-10-02 12:26:18 -0500

    Versión final del 3er problema

--- Subsecuencia.cpp
+++ Subsecuencia.cpp
@@ -7,11 +7,65 @@
 
 #include <iostream>
 #include <stdio.h>
-#include <strings>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+void EncontrarSubstring(char* subA, char* subB, int x, int y){
+
+  int subT[x+1][y+1];   //donde se almacenarán los valores
+  int tam = 0;       //inicio contador
+  int reng, col;
+
+  for (int i = 0; i <= x; i++) {
+    for (int j = 0; j <= y; j++) {
+      if (i == 0 || j == 0) {
+        subT[i][j] = 0;
+
+      }
+        else if (subA[i-1] == subB[j-1]) {
+          subT[i][j] = subT[i-1][j-1] + 1;
+          if (tam < subT[i][j]) {
+            tam = subT[i][j];
+            reng = i;
+            col = j;
+          }
+        }
+        else{
+          subT[i][j] = 0;
+        }
+    }
+  }
+
+  if (tam == 0) {
+    cout << "No hay un substring en común";
+    return;
+  }
+
+  char* subFinal = (char*)malloc((tam+1) * sizeof(char));
+
+  while (subT[reng][col] != 0) {
+    subFinal[--tam] = subA[reng - 1];
+
+    reng --;
+    col--;
+  }
+
+  int subFinalNo = strlen(subFinal);
+
+  cout << "El substring mas grande es: " << subFinal << " y contiene " << subFinalNo << " caracteres." << endl;
+};
 
 int main() {
 
+  char subA[] = "Juanito";
+  char subB[] = "Juanfra";
 
+  int x = strlen(subA);
+  int y = strlen(subB);
+
+  EncontrarSubstring(subA, subB, x, y);
 
   return 0;
 }
